eb9bcba90cd6ea83100ea6b4eb2f80cf79003634
kmiech <48112636+kmiech@users.noreply.github.com>
2023-12-13 20:54:06 -0800

    3 tier architecture working

--- T4S_Webpage/T4S-MODEL/MODEL.cs
+++ T4S_Webpage/T4S-MODEL/MODEL.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace T4S_MODEL
 {
-    public class MODEL
+    public class SignUpModel
     {
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+       
+        public string message { get; set; }
     }
+    public class SignInModel
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string message { get; set; }
+    }
+
 }
--- T4S_Webpage/T4S_Webpage/Controllers/HomeController.cs
+++ T4S_Webpage/T4S_Webpage/Controllers/HomeController.cs
@@ -3,11 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using System.Data.SqlClient;
+using T4S_MODEL;
+using T4S_BLL;
 
 namespace T4S_Webpage.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult SignUp(SignUpModel model)
+        {
+
+            if (string.IsNullOrWhiteSpace(model.userName))
+                return View();
+            else
+            {
+                BLL userSignUp = new BLL();
+                string message = userSignUp.SignUp(model);
+
+
+                return RedirectToAction("SignUpResults", model);
+            }
+
+        }
+
+        public ActionResult SignUpResults(SignUpModel model)
+        {
+            ViewBag.message = model.message;
+            return View();
+        }
+
+        public ActionResult SignIn(SignInModel model)
+        {
+
+            if (string.IsNullOrWhiteSpace(model.userName))
+                return View();
+            else
+            {
+                BLL userSignIn = new BLL();
+                string message = userSignIn.SignIn(model);
+
+
+                return RedirectToAction("SignInResults", model);
+            }
+        }
+
+
+        public ActionResult SignInResults(SignInModel model)
+        {
+            ViewBag.message = model.message;
+            return View();
+        }
         public ActionResult Index()
         {
             return View();
